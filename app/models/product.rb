d73f81b5dce42ee159911ae38a163897fdd2c677
Beer0r <beer0r@gmail.com>
2011-03-28 21:09:57 +0200

    Update

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+validates :titel, :description, :image_url, :presence => true
+validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+validates :titel, :uniqueness => true
+validates :image_url, :format => {
+:with => %r{\.(gif|jpg|png)$}i,
+:message => 'must be a URL for GIF, JPG or PNG image.'
+}
 end
