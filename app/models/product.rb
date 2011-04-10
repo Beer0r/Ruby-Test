1af5e892b824a939143deb90dd2c81ef45c88e2e
Beer0r <beer0r@gmail.com>
2011-04-10 19:38:15 +0200

    Cart update

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,16 @@
 class Product < ActiveRecord::Base
 default_scope :order => 'titel'
+has_many :line_items
+before_destroy :ensure_not_referenced_by_any_line_item
+# ensure that there are no line items referencing this product
+def ensure_not_referenced_by_any_line_item
+if line_items.count.zero?
+return true
+else
+errors[:base] << "Line Items present"
+return false
+end
+end
 validates :titel, :description, :image_url, :presence => true
 validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 validates :titel, :uniqueness => true
