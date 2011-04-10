dc96aae129e5da6531945a9d828ed65871399303
Beer0r <beer0r@gmail.com>
2011-04-10 17:01:03 +0200

    Stylesheet

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+default_scope :order => 'titel'
 validates :titel, :description, :image_url, :presence => true
 validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 validates :titel, :uniqueness => true
