92dff95d25d641df7b53959d2a801da2a228beb0
Beer0r <beer0r@gmail.com>
2011-06-06 21:52:30 +0200

    Fixed Bug in views - 3

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -12,19 +12,19 @@ class CartsController < ApplicationController
 
   # GET /carts/1
   # GET /carts/1.xml
-def show
-begin
-@cart = Cart.find(params[:id])
-rescue ActiveRecord::RecordNotFound
-logger.error "Attempt to access invalid cart #{params[:id]}"
-redirect_to store_url, :notice => 'Invalid cart'
-else
-respond_to do |format|
-format.html # show.html.erb
-format.xml { render :xml => @cart }
-end
-end
-end
+  def show
+    begin
+      @cart = Cart.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, :notice => 'Invalid cart'
+    else
+      respond_to do |format|
+        format.html # show.html.erb
+        format.xml  { render :xml => @cart }
+      end
+    end
+  end
 
   # GET /carts/new
   # GET /carts/new.xml
@@ -76,14 +76,15 @@ end
 
   # DELETE /carts/1
   # DELETE /carts/1.xml
-def destroy
-@cart = Cart.find(params[:id])
-@cart.destroy
-session[:cart_id] = nil
-respond_to do |format|
-format.html { redirect_to(store_url,
-:notice => 'Your cart is currently empty') }
-format.xml { head :ok }
-end
-end
+  def destroy
+    @cart = current_cart
+    @cart.destroy
+    session[:cart_id] = nil
+
+    respond_to do |format|
+      format.html { redirect_to(store_url,
+        :notice => 'Your cart is currently empty') }
+      format.xml  { head :ok }
+    end
+  end
 end
